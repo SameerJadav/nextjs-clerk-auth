b90dd2313cf772121d1c335dcdc20c5957f526b9
SameerJadav <sameerjadav010@gmail.com>
2023-03-27 14:09:45 +0530

    Displaying username

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -5,10 +5,17 @@ import { SignInButton, useUser, SignOutButton } from "@clerk/nextjs";
 export default function SignIn() {
   const user = useUser();
 
+  //   console.log("user", user);
+
   return (
     <main>
-      {!user.isSignedIn && <SignInButton />}
-      {!!user.isSignedIn && <SignOutButton />}
+      {!user.isSignedIn && <SignInButton mode="modal" />}
+      {!!user.isSignedIn && (
+        <>
+          <p>Welcome, {user.user.username}!</p>
+          <SignOutButton />
+        </>
+      )}
     </main>
   );
 }
